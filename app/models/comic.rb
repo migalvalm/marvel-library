ab98b153d26d79d019b7ebd646c1aa561a4a2de4
miguelalvalm <miguelalvalmeida@gmail.com>
2023-12-14 18:25:15 +0000

    [T0-8] Add annotation for models schema

--- app/models/comic.rb
+++ app/models/comic.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: comics
+#
+#  id         :bigint           not null, primary key
+#  characters :string           default([]), is an Array
+#  cover_url  :string
+#  title      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Comic < ApplicationRecord
   # The Marvel Comics API does not provide full paths to images. 
   # Instead, images are represented as a partial path to an image file and the canonical extension of that file.
--- test/models/comic_test.rb
+++ test/models/comic_test.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: comics
+#
+#  id         :bigint           not null, primary key
+#  characters :string           default([]), is an Array
+#  cover_url  :string
+#  title      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 require "test_helper"
 
 class ComicTest < ActiveSupport::TestCase
