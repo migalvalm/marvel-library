ab8dd8ccdecb8d627df1c727bec35a29353738d3
miguelalvalm <miguelalvalmeida@gmail.com>
2023-12-14 21:54:07 +0000

    [T-11] Stylize pagination buttons

--- app/controllers/comics_controller.rb
+++ app/controllers/comics_controller.rb
@@ -2,8 +2,11 @@ class ComicsController < ApplicationController
   def index
     @comics = Comic.page(page).order(published_at: :desc)
 
+    # Checks if user is at the last page
     if @comics.total_pages == page.to_i
+      # Fetch more 100 comics
       Marvel.fetch_comics
+      # Reload the comics to include the newly fetched ones
       @comics = Comic.page(page).order(published_at: :desc)
     end
   end
