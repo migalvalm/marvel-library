1695422d3f29b08da7669c89c3f14354378757c0
miguelalvalm <miguelalvalmeida@gmail.com>
2023-12-14 18:20:28 +0000

    [T-01] config tailwind

---
+++ config/tailwind.config.js
@@ -0,0 +1,27 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: [
+    './public/*.html',
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+    './app/views/**/*.{erb,haml,html,slim}'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'red': '#d63933',
+        'black': '#2b2b2b'
+      },
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif']
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/container-queries'),
+  ]
+}
